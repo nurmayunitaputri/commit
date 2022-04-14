aefbe2c37c9e2607cb580d5dad17447cfc8c8534
Sheilla Sara Yasmine <sheilayasmine12@gmail.com>
2022-04-14 22:35:30 +0700

    push login

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -3,7 +3,7 @@ const Input = ({ label, name, type, onChange, onBlur, placeholder, dataTestId })
   return (
     <label htmlFor={name} className="block w-full mb-1">
       <div className="font-bold mb-1">{label}</div>
-      <input type={type} name={name} className="rounded-lg p-2 text-sm  border  max-h-11 border-zinc-900 focus:outline-none w-full"  placeholder={placeholder} onChange={onChange} onBlur={onBlur} data-testid={dataTestId} />
+      <input type={type} name={name} className="rounded-lg p-2 text-sm  border  max-h-11 border-zinc-900 focus:outline-none  w-full"  placeholder={placeholder} onChange={onChange} onBlur={onBlur} data-testid={dataTestId} />
     </label>
   );
 };
